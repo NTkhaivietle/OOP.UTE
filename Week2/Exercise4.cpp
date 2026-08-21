308d4e1dbc7ac5606db667bb9585348d9b90a24f
Khai Le <khaivietle@gmail.com>
2026-08-21 12:03:35 +0700

    Add Week2 exercise

---
+++ Week2/Exercise4.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    // Student 1
+    std::string studentName1 = "Nguyen Van A";
+    std::string studentId1 = "23110001";
+    std::string studentPhone1 = "0901234567";
+
+    // Student 2
+    std::string studentName2 = "Tran Thi B";
+    std::string studentId2 = "23110002";
+    std::string studentPhone2 = "0923456789";
+
+    // Student 3
+    std::string studentName3 = "Le Van C";
+    std::string studentId3 = "23110003";
+    std::string studentPhone3 = "0934567890";
+
+    // Student 4
+    std::string studentName4 = "Pham Thi D";
+    std::string studentId4 = "23110004";
+    std::string studentPhone4 = "0945678901";
+
+    // Student 5
+    std::string studentName5 = "Hoang Van E";
+    std::string studentId5 = "23110005";
+    std::string studentPhone5 = "0956789012";
+
+    // Student 6
+    std::string studentName6 = "Vo Thi F";
+    std::string studentId6 = "23110006";
+    std::string studentPhone6 = "0967890123";
+
+    // Student 7
+    std::string studentName7 = "Dang Van G";
+    std::string studentId7 = "23110007";
+    std::string studentPhone7 = "0978901234";
+
+    // Student 8
+    std::string studentName8 = "Bui Thi H";
+    std::string studentId8 = "23110008";
+    std::string studentPhone8 = "0989012345";
+
+    // Student 9
+    std::string studentName9 = "Do Van I";
+    std::string studentId9 = "23110009";
+    std::string studentPhone9 = "0901122334";
+
+    // Student 10
+    std::string studentName10 = "Nguyen Thi K";
+    std::string studentId10 = "23110010";
+    std::string studentPhone10 = "0912233445";
+
+    // Student 11
+    std::string studentName11 = "Tran Van L";
+    std::string studentId11 = "23110011";
+    std::string studentPhone11 = "0923344556";
+
+    // Student 12
+    std::string studentName12 = "Le Thi M";
+    std::string studentId12 = "23110012";
+    std::string studentPhone12 = "0934455667";
+
+    // Student 13
+    std::string studentName13 = "Pham Van N";
+    std::string studentId13 = "23110013";
+    std::string studentPhone13 = "0945566778";
+
+    // Student 14
+    std::string studentName14 = "Hoang Thi O";
+    std::string studentId14 = "23110014";
+    std::string studentPhone14 = "0956677889";
+
+    // Student 15
+    std::string studentName15 = "Vo Van P";
+    std::string studentId15 = "23110015";
+    std::string studentPhone15 = "0967788990";
+
+    // Student 16
+    std::string studentName16 = "Dang Thi Q";
+    std::string studentId16 = "23110016";
+    std::string studentPhone16 = "0978899001";
+
+    // Student 17
+    std::string studentName17 = "Bui Van R";
+    std::string studentId17 = "23110017";
+    std::string studentPhone17 = "0989900112";
+
+    // Student 18
+    std::string studentName18 = "Do Thi S";
+    std::string studentId18 = "23110018";
+    std::string studentPhone18 = "0901011223";
+
+    // Student 19
+    std::string studentName19 = "Nguyen Van T";
+    std::string studentId19 = "23110019";
+    std::string studentPhone19 = "0912122334";
+
+    // Student 20
+    std::string studentName20 = "Tran Thi U";
+    std::string studentId20 = "23110020";
+    std::string studentPhone20 = "0923233445";
+
+    return 0;
+}
