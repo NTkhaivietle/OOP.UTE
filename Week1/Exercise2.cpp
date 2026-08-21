728d471954ae7d1f9d2b93e395010ad887c65cb5
Khai Le <khaivietle@gmail.com>
2026-08-21 10:30:18 +0700

    Add Week1 exercises

---
+++ Week1/Exercise2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    //1. Talking about myself
+    cout << " =======================\n";
+    cout << "    My First C++ APP   \n";
+    cout << " =======================\n";
+    cout << "Name: Khai Le\n";              
+    cout << "Course: OOP with C++\n";
+    cout << "School: HCMUTE\n" << endl;
+
+    //2. About my hoppings
+    cout << "My favorite language: Python\n";     // it's much easier to learn than C++ but I still love C++ because it's very powerful
+    cout << "My goal: become a security engineer\n";
+    cout << "My message: Never stop learning!\n"<< endl;
+
+    cout << "********************************\n";
+    cout << "    KEEP CODING 🚀  \n";
+    cout << "********************************\n";
+    return 0;
+}
